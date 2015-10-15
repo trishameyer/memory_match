ae3ddeaf565491ef7bd09593831ce9290a7cdb54
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-10-14 18:38:52 -0700

    Memory Match v0.5 - Stefanie Yoshimura

--- main.js
+++ main.js
@@ -5,16 +5,24 @@ var match_counter = 0;
 
 $(document).ready(function () {
 
-    $(".back").click(function () {
+    $(".back").click(card_clicked);
+
+    function card_clicked() {
         $(this).hide();
 
-        if (first_card_clicked = null) {
-            first_card_clicked = $(this).children("img").attr("src");
+        if (first_card_clicked == null) {
+            first_card_clicked = $(this).prev().find("img").attr("src");
+            console.log(first_card_clicked);
+            return first_card_clicked;
         }
 
-        console.log(first_card_clicked);
-    });
+        else {
+            second_card_clicked = $(this).prev().find("img").attr("src");
+            console.log(second_card_clicked);
+            return second_card_clicked;
+        }
 
+    };
 });
 
 
