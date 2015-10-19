8cae3cc1a4ec9bf023b3fddc41d888df2b075c7b
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-10-18 19:11:21 -0700

    Memory Match v1.0 - Stefanie Yoshimura

--- main.js
+++ main.js
@@ -2,10 +2,35 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 9;
 var match_counter = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
 
-$(document).ready(function () {
+$(function () {
 
-    $(".back").click(card_clicked);
+    function display_stats() {
+        accuracy = Math.round(100*(match_counter / attempts));
+        $(".games-played .value").text(games_played);
+        $(".attempts .value").text(attempts);
+        if (match_counter == 0 && attempts == 0) {
+            $(".accuracy .value").text("100%");
+        }
+        //else if (match_counter == 1 && attempts == 1)  {
+        //    $(".accuracy .value").text("100%");
+        //}
+        else {
+            $(".accuracy .value").text(accuracy + "%");
+        }
+    }
+
+    function reset_stats() {
+        games_played = games_played + 1;
+        accuracy = 0;
+        match_counter = 0;
+        attempts = 0;
+        $(".back").show();
+        display_stats();
+    }
 
     function card_clicked() {
         $(this).addClass("selected_card");
@@ -19,10 +44,12 @@ $(document).ready(function () {
 
         else {
             second_card_clicked = $(this).prev().find("img").attr("src");
+            attempts = attempts + 1;
             console.log(second_card_clicked);
 
             if (first_card_clicked == second_card_clicked) {
                 match_counter = match_counter + 1;
+                display_stats();
                 first_card_clicked = null;
                 second_card_clicked = null;
                 console.log(first_card_clicked);
@@ -39,6 +66,7 @@ $(document).ready(function () {
                 $(".back").unbind();
                 setTimeout(function () {
                     $(".selected_card").show();
+                    display_stats();
                     first_card_clicked = null;
                     second_card_clicked = null;
                     console.log(first_card_clicked);
@@ -51,7 +79,14 @@ $(document).ready(function () {
 
         }
 
-    };
+    }
+
+    $(".back").click(card_clicked);
+
+    $(".reset").click(reset_stats);
+
+    display_stats();
+
 });
 
 
