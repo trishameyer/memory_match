f17489f2bd8fba6dad1cf1ac2a1eb2da24c642d3
RhettDelFierro <thinquetank@gmail.com>
2015-10-19 09:57:34 -0700

    Memory Match v1.0 - Rhett Del Fierroo

--- javascript.js
+++ javascript.js
@@ -3,19 +3,19 @@
  */
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
 var matches =0;
 var attempts = 0;
 var accuracy = 0;
+var games_played = 0;
 
 
 
 $( document ).ready(function() {
     $(".back").click(card_clicked);
     $(".back").click(display_stats);
-    games_played = 0;
-    $(".reset").click(function {
+    $(".reset").click(function() {
         games_played++;
         reset_stats();
         display_stats();
@@ -23,17 +23,18 @@ $( document ).ready(function() {
 });
 
 function card_clicked(event) {
-    $(this).hide(); //could put $(this).toggleClass('someclass that affects css') instead.
+    $(this).addClass('hideCard'); //could put $(this).toggleClass('someclass that affects css') instead.
     console.log('event objects worked!');
     if (first_card_clicked == null) {
         first_card_clicked = $(this).prev().find('img').attr('src'); //do I need to  get the image source for the DOM?
     } else {
         second_card_clicked = $(this).prev().find('img').attr('src');
-        attempts++;//again, see above comment.
-        accuracy = matches/attempts;
+        //again, see above comment.
+ //       accuracy = matches/attempts;
         if (first_card_clicked === second_card_clicked) {
             match_counter++;
             matches++;
+            attempts++;
             console.log('match_counter is: ' + match_counter);
             first_card_clicked = null;
             second_card_clicked = null;
@@ -44,10 +45,13 @@ function card_clicked(event) {
             }
         } else {
             console.log('first_card_clicked != second_card_clicked');
-            $('.back').show(2000); //could put $('.back').toggleClass('someclass that affects css') instead to flip it back.
+            $('hideCard').toggleClass('showCard'); //could put $('.back').toggleClass('someclass that affects css') instead to flip it back.
+            //$(second_card_clicked).toggleClass('showCar')
             first_card_clicked = null;
             second_card_clicked = null;
             console.log('click handler functionality is complete - the second');
+            attempts++;
+            //should add a card_clicked class to the ones with back flipped.
         }
     }
 }
@@ -55,7 +59,7 @@ function card_clicked(event) {
 function display_stats(event) {
     $(".games_played .value").text(games_played);
     $('.attempts .value').text(attempts);
-    accuracy = Math.round((matches/attempts)) + "%"
+    accuracy = (Math.round((matches/attempts)) * 100) + "%"
     $('.accuracy .value').text(accuracy);
 }
 
