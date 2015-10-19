c8291a59c1a5268236a73fce6646d8b7e290569c
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-10-19 01:47:04 -0700

    Memory Match v1.0 - Ryan Transfiguracion

--- match_logic.js
+++ match_logic.js
@@ -4,10 +4,18 @@ var match_counter = 0;
 var card_pair_flipped = false;
 var total_card_matches = 9;
 
+// Stats variables
+var matches = 0;        // Increments when a matching pair is found
+var attempts = 0;       // Increments when the player selects a pair of cards (matching or not).
+var accuracy = 0;       // Quotient of matches and attempts.
+var games_played = 0;   // Increments when the reset button is pressed.
 
 $(document).ready(function()
 {
-   console.log("Excuse me while I kiss the sky.");
+    console.log("Excuse me while I kiss the sky.");
+
+
+    init_display_stats();
 
     $("div#game-area>div.card").click(cardClicked);
     //document.querySelector("#game-area>div.card").addEventListener("click", cardClicked);
@@ -17,6 +25,7 @@ $(document).ready(function()
 function cardClicked()
 {
     console.log("A card was clicked!!!");
+    reset_clicked = false;
 
     if (card_pair_flipped)
     {
@@ -47,10 +56,13 @@ function cardClicked()
         rotateCard(second_card_clicked);
         makeCardUnclickable(second_card_clicked);
         card_pair_flipped = true;
+        attempts++;
 
         // Flowchart - first_card_clicked is equal to second_card_clicked
         if (checkCardsMatch())      // YES
         {
+            matches++;
+
             // Allow other cards to be flipped
             card_pair_flipped = false;
 
@@ -61,7 +73,7 @@ function cardClicked()
                 // Flowchart - Display to the user that they have won
                 console.log("YOU WON!!!!");
                 winGame();
-                return;
+                //return;
             }
         }
         else                        // NO
@@ -80,6 +92,8 @@ function cardClicked()
                 },
                 2000);
         }
+
+        display_stats();
         // Flowchart - if pair doesn't match or not all cards matched
         // Reset variables
         resetClickedCards();
@@ -157,6 +171,11 @@ function resetGame()
     second_card_clicked = null;
     match_counter = 0;
 
+    games_played++;
+
+    reset_stats();
+    display_stats();
+
     // Make the cards visible and showing their back faces
     $("div.card").removeClass("make-disappear");
     $("div.card>span").removeClass("rotate-card-hide");
@@ -173,3 +192,52 @@ function resetGame()
 
     console.log("resetGame called!!!");
 }
+
+/* VERSION 1.0 FUNCTIONS BELOW */
+// helper function for when the page loads
+function init_display_stats()
+{
+    // Add p element to display Games Played
+    var temp = $("<p>");
+    temp.append(games_played);
+    var gp_div = $('.games-played>.value');
+    gp_div.append(temp);
+
+    // Add p element to display attempts
+    temp = $("<p>");
+    temp.append(attempts);
+    var attempts_div = $('.attempts>.value');
+    attempts_div.append(temp);
+
+    // Add p element to display accuracy
+    temp = $("<p>");
+    temp.append(accuracy.toFixed(2) + '%');
+    var accuracy_div = $('.accuracy>.value');
+    accuracy_div.append(temp);
+}
+function display_stats()
+{
+    // Replace games played number
+    $('.games-played>.value').find('p').text(games_played);
+
+    // Replace attempts number
+    $('.attempts>.value').find('p').text(attempts);
+
+    // Replace accuracy number
+    // Protect against division by zero
+    if (attempts == 0)
+        accuracy = 0;
+    else
+        accuracy = matches / attempts;
+    var acc = (accuracy * 100).toFixed(2);
+    $('.accuracy>.value').find('p').text(acc + '%');
+}
+
+function reset_stats()
+{
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+}
+
+/* END VERSION 1.0 FUNCTIONS */
