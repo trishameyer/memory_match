c444b61eb3bd2d15f33a26a5715f1aedcb77a712
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-10-20 14:53:46 -0700

    Memory Match v1.0c - Stefanie Yoshimura

--- main.js
+++ main.js
@@ -5,96 +5,100 @@ var match_counter = 0;
 var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
+function card_effect(element) {
+    $(element).toggleClass("back_effect");
+    $(element).prev(".front").toggleClass("front_effect");
+}
 
-$(function () {
+function display_stats() {
+    accuracy = Math.round(100 * (match_counter / attempts));
+    $(".games-played .value").text(games_played);
+    $(".attempts .value").text(attempts);
 
-    function card_effect(element) {
-        $(element).toggleClass("back_effect");
-        $(element).prev(".front").toggleClass("front_effect");
+    if (match_counter == 0 && attempts == 0) {
+        $(".accuracy .value").text("100%");
+    }
+    else {
+        $(".accuracy .value").text(accuracy + "%");
+    }
+}
+
+function reset_stats() {
+    $(".back").show();
+    $(".matched").removeClass("matched");
+    $("div").removeClass("back_effect").removeClass("front_effect").removeClass("selected_card");
+    games_played = games_played + 1;
+    accuracy = 0;
+    match_counter = 0;
+    attempts = 0;
+    display_stats();
+}
+
+function card_clicked(_this) {
+    if ($(_this).hasClass("matched") == true || $(_this).hasClass("selected_card") == true) {
+        return
+    }
+    $(_this).addClass("selected_card");
+    $(_this).off("click");
+    card_effect(_this);
+    setTimeout(function () {
+        $(_this).hide();
+    }, 2000);
+
+    if (first_card_clicked == null) {
+        first_card_clicked = $(_this).prev().find("img").attr("src");
+        return first_card_clicked;
     }
 
-    function display_stats() {
-        accuracy = Math.round(100 * (match_counter / attempts));
-        $(".games-played .value").text(games_played);
-        $(".attempts .value").text(attempts);
+    else {
+        second_card_clicked = $(_this).prev().find("img").attr("src");
+        attempts = attempts + 1;
+        console.log(second_card_clicked);
 
-        if (match_counter == 0 && attempts == 0) {
-            $(".accuracy .value").text("100%");
-        }
-        else {
-            $(".accuracy .value").text(accuracy + "%");
-        }
-    }
-
-    function reset_stats() {
-        $(".back").show().click(card_clicked);
-        $(".matched").removeClass("matched");
-        $("div").removeClass("back_effect").removeClass("front_effect").removeClass("selected_card");
-        games_played = games_played + 1;
-        accuracy = 0;
-        match_counter = 0;
-        attempts = 0;
-        display_stats();
-    }
-
-    function card_clicked() {
-        if ($(this).hasClass("matched") == true) {
-            return
-        }
-        $(this).addClass("selected_card");
-        $(this).off("click");
-        card_effect(this);
-        setTimeout(function () {
-            $(this).hide();
-        }, 2000);
-
-        if (first_card_clicked == null) {
-            first_card_clicked = $(this).prev().find("img").attr("src");
-            return first_card_clicked;
-        }
-
-        else {
-            second_card_clicked = $(this).prev().find("img").attr("src");
-            attempts = attempts + 1;
+        if (first_card_clicked == second_card_clicked) {
+            match_counter = match_counter + 1;
+            first_card_clicked = null;
+            second_card_clicked = null;
+            console.log(first_card_clicked);
             console.log(second_card_clicked);
+            console.log(match_counter);
 
-            if (first_card_clicked == second_card_clicked) {
-                match_counter = match_counter + 1;
-                display_stats();
+            if (match_counter == total_possible_matches) {
+                alert("You won! It's working! Yay!");
+            }
+        }
+
+        else {
+            $(".back").off("click", card_clicked());
+            setTimeout(function () {
+                card_effect(".selected_card");
+                $(".selected_card").show();
                 first_card_clicked = null;
                 second_card_clicked = null;
                 console.log(first_card_clicked);
                 console.log(second_card_clicked);
                 console.log(match_counter);
-                $(".selected_card").addClass("matched").removeClass("selected_card");
-                if (match_counter == total_possible_matches) {
-                    alert("You won! It's working! Yay!");
-                }
-            }
-
-            else {
-                $(".back").off("click");
-                setTimeout(function () {
-                    card_effect(".selected_card");
-                    $(".selected_card").show();
-                    display_stats();
-                    first_card_clicked = null;
-                    second_card_clicked = null;
-                    console.log(first_card_clicked);
-                    console.log(second_card_clicked);
-                    console.log(match_counter);
-                    $(".back").removeClass("selected_card").click(card_clicked);
-                }, 1200);
-
-            }
+                $(".back").removeClass("selected_card").on("click", card_clicked());
+            }, 1200);
 
         }
+        display_stats();
 
     }
 
-    $(".back").click(card_clicked);
+}
 
-    $(".reset").click(reset_stats);
+$(document).ready(function () {
+
+
+
+    $(".back").click(function(){
+        card_clicked(this);
+    });
+
+    $(".reset").click(function(){
+        reset_stats();
+    });
 
     display_stats();
 
