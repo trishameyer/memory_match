eb725c378fc093ba88384fb84e6f6818d6d63864
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-10-26 15:20:23 -0700

    Tweaks and image updates

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function display_stats() {
 }
 
 function reset_stats() {
-    $(".back").show();//.on("click", card_clicked);
+    $(".back").show();
     $("div").removeClass("back_effect").removeClass("front_effect").removeClass("selected_card").removeClass("matched");
     games_played = games_played + 1;
     accuracy = 0;
@@ -39,11 +39,7 @@ function card_clicked(_this) {
         return
     }
     $(_this).addClass("selected_card");
-    //$(_this).off("click");
     card_effect(_this);
-    //setTimeout(function () {
-    //    $(_this).hide();
-    //}, 2000);
 
     if (first_card_clicked == null) {
         first_card_clicked = $(_this).prev().find("img").attr("src");
@@ -53,20 +49,16 @@ function card_clicked(_this) {
     else {
         second_card_clicked = $(_this).prev().find("img").attr("src");
         attempts = attempts + 1;
-        console.log(second_card_clicked);
 
         if (first_card_clicked == second_card_clicked) {
             match_counter = match_counter + 1;
             first_card_clicked = null;
             second_card_clicked = null;
-            console.log(first_card_clicked);
-            console.log(second_card_clicked);
-            console.log(match_counter);
             $(".selected_card").addClass("matched");
             $("div").removeClass("selected_card");
 
             if (match_counter == total_possible_matches) {
-                alert("You won! It's working! Yay!");
+                alert("You won!");
             }
         }
 
@@ -77,9 +69,6 @@ function card_clicked(_this) {
                 $(".selected_card").show();
                 first_card_clicked = null;
                 second_card_clicked = null;
-                console.log(first_card_clicked);
-                console.log(second_card_clicked);
-                console.log(match_counter);
                 $(".back").removeClass("selected_card").on("click", card_clicked);
             }, 1500);
 
