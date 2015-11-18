865fddc874a0960d02bcc245957c0f27db27c635
RhettDelFierro <thinquetank@gmail.com>
2015-11-18 10:28:26 -0800

    chaning some loops

--- javascript.js
+++ javascript.js
@@ -1,86 +1,15 @@
-///**
-// * Created by Rhett on 10/19/2015.
-// */
-//var first_card_clicked = null;
-//var second_card_clicked = null;
-//var total_possible_matches = 9;
-//var match_counter = 0;
-//var matches =0;
-//var attempts = 0;
-//var accuracy = 0;
-//
-//$( document ).ready(function() {
-//    $(".back").click(card_clicked);
-//    $(".back").click(display_stats);
-//    $(".reset").click(function() {
-//        games_played++;
-//        reset_stats();
-//        display_stats();
-//    });
-//    games_played = 0;
-//    reset_stats();
-//});
-//
-//function card_clicked(event) {
-//    $(this).hide().addClass('card_selected'); //could put $(this).toggleClass('someclass that affects css') instead.
-//    console.log('clicked');
-//    if (first_card_clicked == null) {
-//        first_card_clicked = $(this).prev().find('img').attr('src');
-//    } else {
-//        second_card_clicked = $(this).prev().find('img').attr('src');
-//
-//        if (first_card_clicked === second_card_clicked) {
-//            match_counter++;
-//            matches++;
-//            attempts++;
-//            console.log('match_counter is: ' + match_counter);
-//            first_card_clicked = null;
-//            second_card_clicked = null;
-//            $('.card_selected').removeClass('card_selected');
-//            console.log("class name: "+ this.className);
-//            if (match_counter === total_possible_matches) {
-//                alert('You have won!');
-//                reset_stats();
-//            } else {
-//                return console.log('click handler functionality is complete - the first one');
-//            }
-//        }
-//        else {
-//            console.log('first_card_clicked != second_card_clicked');
-//            first_card_clicked = null;
-//            second_card_clicked = null;
-//            console.log('click handler functionality is complete - the second');
-//            attempts++;
-//            $('.card_selected').show(2000);
-//         //   wait = setTimeout(function() {
-//           //     $('.card_selected').show()
-//           // }, 2000);
-//        }
-//    }
-//}
-//
-//function display_stats() {
-//    $(".games_played .value").text(games_played);
-//    $('.attempts .value').text(attempts);
-//    accuracy = Math.round((matches/attempts) * 100);
-//    if (isNaN(accuracy)) {
-//        accuracy = 0;
-//    }
-//    $('.accuracy .value').text(accuracy + '%');
-//}
-//
-//function reset_stats() {
-//    accuracy = 0;
-//    matches = 0;
-//    attempts = 0;
-//    display_stats();
-//    $('.back').show();
-//}
-
 var game_area = $('<div>').attr('id', 'game-area');
 var front = '';
-var card_array = ["michaeljackson", "thebeatles", "ladygaga", "pharrell",
-    "atcq", "lanadelrey", "whitneyhouston", "drake", "edsheeran"];
+var current_set='peace';
+var card_sets = {
+    peace: ["michaeljackson", "thebeatles", "ladygaga", "pharrell",
+        "atcq", "lanadelrey", "whitneyhouston", "drake", "edsheeran"],
+    war: ['jasonbourne', 'jackreacher', 'eli', 'johnwick', 'taken',
+        'alejandro', 'jamesbond', 'jaqen', 'omar']
+};
+{
+    card_sets[current_set]
+}
 function CardConstructor(artist) {
     var self = this;
     self.artist = artist;
@@ -114,15 +43,7 @@ function CardConstructor(artist) {
         }
     };
     board.remove_half();
-    /**
-     *
-     * @param card clicked to check and eliminate.
-     */
-//self.card_clicked = function (event) {
-//    self.clicked = true;
-//
-//
-//};
+
 }
 
 //ok keep it how it is, except make the card array twice as long and append from that (or run the loop to randomize twice). Then run a loop in the board object from the cards constructor to remove duplicates.
@@ -135,63 +56,60 @@ function Board_Constructor(array) {
     self.new_array = [];
     self.cards = array;
     self.accuracy = 0;
+    self.new_array2 = [];
 
     self.randomize = function (array) { //now we have a new array posted in this.new_array.
         console.log('called');
         self.create_board(game_area);
-        for (o = 0; o < 2; o++) {
-            for (var i = array.length - 1; i > 0; i--) { //randomizes loaded array.
-                var j = Math.floor(Math.random() * (i + 1));
-                var temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-            for (var t = 0; t < array.length; t++) { //gets done twice -> for card image array.
-                self.new_array.push(new CardConstructor(array[t]));
-            }
+        //self.new_array = array.slice(Math.floor(Math.random()*9));
+        self.new_array = array.slice();
+        for (var o = 0; o < self.new_array.length; o++){
+            self.new_array.push(self.new_array[o]);
         }
-
+        var index;
+        for(i=0; i < self.new_array.length; i++){
+            index = Math.floor(Math.random() * self.new_array.length);
+            self.new_array2.push(self.new_array[index]);
+            self.new_array.splice(index, 1);
+        }
+        for (var t = 0; t < self.new_array2.length; t++){
+            var card = new CardConstructor(self.new_array2[t])
+        }
+        //copy the array
+        //loop until the array is empty
+        //pick a random element from the current array length
+        //put that element into a new array
+        //remove the same element from the old array
+        //for (o = 0; o < 2; o++) {
+        //    self.new_array = array.slice(Math.floor(Math.random()*9));
+            //for (var i = array.length - 1; i > 0; i--) { //randomizes loaded array.
+            //    var j = Math.floor(Math.random() * (i + 1));
+            //    var temp = array[i];
+            //    array[i] = array[j];
+            //    array[j] = temp;
+            //}
+            //for (var t = 0; t < array.length; t++) { //gets done twice -> for card image array.
+            //    self.new_array2.push(new CardConstructor(array[t]));
+            //}
     };
 
     self.remove_half = function () {
-        self.new_array.splice(self.cards.length - 1, self.cards.length);
-    };
-
-    self.catch_from_array = function (front) {
-        for (i = 0; i < self.new_array.length; i++) {
-            if (front === self.new_array[i].artist) {
-                self.new_array[i].clicked = true;
-                //card_object_array.splice(i);
-                break;
-            }
-        }
-    };
-
-    self.check_from_array = function (front) {
-        for (i = 0; i < self.new_array.length; i++) {
-            if (front === self.new_array[i].artist) {
-                if (self.new_array[i].clicked === true) {
-                    return true;
+        for (var o = 0; o < self.new_array2.length; o++) {
+            for (var i = 0; i < self.new_array2.length; i++) {
+                if (self.new_array2[o].artist ===self.new_array2[i].artist){
+                    self.new_array2.splice(i,1);
                 }
             }
         }
-        return false;
     };
 
-    self.set_false = function (front) {
-        for (i = 0; i < self.new_array.length; i++) {
-            if (front === self.new_array[i].artist) {
-                self.new_array[i].clicked = false;
-            }
-        }
-    };
-    self.create_board = function (game_area) {
+    self.create_board = function (game_area, option) {
         console.log('create_board is called');
         var complete_container = $('<div>').addClass('container-fluid');
         //header
         var header = $('<div>').addClass('jumbotron');
         var logo = $('<img>').attr("src", "http://thumbs.dreamstime.com/x/music-logo-13731704.jpg");
-        var h1 = $('<h1>').text('Music Theme');
+        var h1 = $('<h1>').text(option);
 
         header.append(logo, h1);
         complete_container.append(header);
@@ -219,8 +137,10 @@ function Board_Constructor(array) {
         accuracy.append(label.text('Accuracy'), accuracy_paragraph);
 
         var reset_button = $('<button>').addClass('reset').text('reset Game');
+        var button_peace = $('<button>').addClass('btn btn-success peace').text('Peace');
+        var button_war = $('<button>').addClass('btn btn-danger war').text('War');
 
-        sidebar_wrapper.append(games_played, attempts, accuracy, reset_button);
+        sidebar_wrapper.append(games_played, attempts, accuracy, reset_button, 'button_'+option);
         stat_container.append(sidebar_wrapper);
         container.append(stat_container);
         //stats area finished.
@@ -235,7 +155,7 @@ function Board_Constructor(array) {
         if (self.matches === self.cards.length){
             alert('you have won!');
         }
-    }
+    };
 
     self.display_stats = function () {
         $(".games_played .value").text(self.games_played);
@@ -292,21 +212,21 @@ function card_clicked(event) {
 }
 
 $(document).ready(function () {
-    board = new Board_Constructor(card_array);
+    board = new Board_Constructor(card_sets[current_set]);
     //board.create_board(game_area);
-    board.randomize(card_array);
+    board.randomize(card_array_peace);
     $(".back").on('click', function () {
         var show = this;
         if (front === '') {
             front = $(this).prev().find('img').attr('picture');
-            for (var i = 0; i < board.new_array.length; i++) {
+            for (var i = 0; i < board.new_array2.length; i++) {
                 if (board.new_array[i].artist === front) {
                     board.new_array[i].clicked_false(show);
                 }
             }
         } else {
             front = $(this).prev().find('img').attr('picture');
-            for (var i = 0; i < board.new_array.length; i++) {
+            for (var i = 0; i < board.new_array2.length; i++) {
                 if (board.new_array[i].artist === front) {
                     board.new_array[i].click_check(show);
                 }
@@ -321,6 +241,18 @@ $(document).ready(function () {
         board.games_played++;
         board.reset_stats();
         board.display_stats();
+    });
+    $('.war').on('click', function(){
+        board.reset_stats();
+        board.display_stats();
+        boardwar = new Board_Constructor(card_array_war, peace);
+        board.randomize(card_array_war);
+    });
+    $('.war').on('click', function(){
+        board.reset_stats();
+        board.display_stats();
+        boardwar = new Board_Constructor(card_array_peace, war);
+        board.randomize(card_array_peace);
     });
     board.games_played = 0;
     board.reset_stats();
