2347e73650497e10081f799ba2aba6c3c5ed3674
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-11-20 18:22:03 -0800

    Revised the getRandomSize function

--- globals.js
+++ globals.js
@@ -7,18 +7,10 @@ var RULE_KEY_ALL_PAIRS_MATCHED = 'all_pairs_matched';
 var FLIP_BACK_DELAY = 2000;
 
 var SIZE_ARRAY = [4, 5, 6, 8, 9];
-var RANDOM_SIZE = true;
 function getRandomSize()
 {
-    if (RANDOM_SIZE == true)
-    {
-        var rand_index = Math.floor(Math.random() * SIZE_ARRAY.length);
-        return SIZE_ARRAY[rand_index];
-    }
-    else
-    {
-        return DEFAULT_NUM_PAIRS;
-    }
+    var rand_index = Math.floor(Math.random() * SIZE_ARRAY.length);
+    return SIZE_ARRAY[rand_index];
 }
 
 var game_ref;
