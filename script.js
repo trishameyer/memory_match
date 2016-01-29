480f8532fba59d3f41a093a253000448d0b7a22d
Trisha Meyer <tmeye62@gmail.com>
2016-01-29 14:49:48 -0800

    added majority of the functionality that allows cards to show/hide and ability to win

--- script.js
+++ script.js
@@ -9,25 +9,49 @@ match_counter = 0;
 
 //Add a click handler to each card using either jQuery-intermediate or onclick attribute-fundamentals
 $(document).ready(function(){
-    $(".back").click(function() {
-        //console.log('card clicked: ',this);
+    //click handler for back of card that calls to function card_clicked
+    $('.card').click(function(){
+        console.log("card clicked: ", this);
         card_clicked(this);
     });
-
-    //$(".front").click(function() {
-    //    $(".front").next().show();
-    //});
-
 });
 
 function card_clicked(the_card){
-    //could i also just use (first_card_clicked) with no condition?
-    if (first_card_clicked == null){
-        first_card_clicked =  $(this).find("img").attr('src');
-        $(the_card).hide();
-    } else {
-        if(first_card_clicked == second_card_clicked){
+    console.log("card_clicked function called", the_card);
 
-        }
-    }
-}
+    if(first_card_clicked == null){
+        first_card_clicked = $(the_card).find('.front').find('img').attr('src');
+        $(the_card).find('.back').hide();
+        console.log("First Card Variable set: ", first_card_clicked);
+    } else {
+        second_card_clicked = $(the_card).find('.front').find('img').attr('src');
+        $(the_card).find('.back').hide();
+        console.log("Second card variable set:", the_card);
+
+        if (first_card_clicked == second_card_clicked){
+            console.log("1st and 2nd card are the same");
+            match_counter++;
+            console.log("Match counter increments to: ", match_counter);
+            first_card_clicked = null;
+            console.log("Reset 1st card value: ", first_card_clicked);
+            second_card_clicked = null;
+            console.log("Reset 1st card value: ", second_card_clicked);
+
+            if (match_counter == total_possible_matches){
+                console.log("You won!"); //somehow show this to user
+            }
+        } else {
+            console.log("cards are not the same");
+            $('.back').delay(2000).show(100);
+            //add some sort of shake to other cards if clicked in this duration
+            first_card_clicked = null;
+            console.log("Reset 1st card value: ", first_card_clicked);
+            second_card_clicked = null;
+            console.log("Reset 1st card value: ", second_card_clicked);
+
+        } //end of 1st card = 2nd card conditional
+
+    } //end of card=null conditional
+
+} //end of function
+
