784d7a0b9191a60a4a9276513f54cc18c994a4db
Trisha Meyer <tmeye62@gmail.com>
2016-02-01 18:10:00 -0800

    stats container works and changed some css

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 //Assigned to the total possible number of matches
 var total_possible_matches = 9;
-match_counter = 0;
+//Declare a variable matches and set to 0
+var matches = 0;
+var attempts = 0;
+var accuracy= 0 + '%'; // matches/attempts %
+var games_played = 0;
 //define a flag variable that controls the ability to click
 var canClick = true;
 var first = null;
@@ -14,9 +18,21 @@ var second = null;
 $(document).ready(function(){
     //click handler for back of card that calls to function card_clicked
     $('.card').click(function(){
-        //console.log("card clicked: ", this);
         card_clicked(this);
     });
+
+    //on page load, define a new global variable called games_played & when reset button is clicked it should increment 1
+    //var games_played; //this didn't show my values at all
+
+    $('.reset').click(function(){
+        games_played++; //not calculating properly when i declare a new variable on page load!
+        reset_stats();
+        display_stats(); //can't get code quite right in working order
+        $('.back').addClass('notFlipped').show();
+        $(new_div).hide(); //if this hasn't showed up yet, you get a reference error because the div hasn't been created yet
+
+    });
+    //display_stats();
 });
 
 
@@ -29,42 +45,66 @@ function card_clicked(the_card){
     $(the_card).find('.back').hide();
     if(first_card_clicked == null){
         first_card_clicked = $(the_card).find('.front').find('img').attr('src');
-        first = $(the_card).find('.back');//.hide(); //added code
+        first = $(the_card).find('.back');
     } else {
         //sets clicked card to variable second_card clicked since it wasn't the first card clicked
         second_card_clicked = $(the_card).find('.front').find('img').attr('src');
-        second = $(the_card).find('.back');//.hide();
+        second = $(the_card).find('.back');
+        //every time a user attempts to match (clicks second card) the attempts increments by 1
+        attempts++;
         //asks if first card clicked is equal to second card click
         if (first_card_clicked == second_card_clicked){
-            //increment match_counter up one and set both card variables equal to null; allows you the ability to click more cards now that the variables are reset
+            //increment matches up one and set both card variables equal to null; allows you the ability to click more cards now that the variables are reset
             canClick = false;
             $(first).removeClass('notFlipped');
             $(second).removeClass('notFlipped');
-            match_counter++;
+            matches++;
             first_card_clicked = null;
             second_card_clicked = null;
             canClick = true;
+            accuracy_percent(matches, attempts);
             //compares number of matched pairs to total possible matches and if they are equal, you win
-            if (match_counter == total_possible_matches){
-                console.log("You won!"); //somehow show this to user
+            if (matches == total_possible_matches){
+                //console.log("You won!"); somehow show this to user
                 new_div = $('<div>').addClass('new_div').text('You Won!');
                 $('.game-area').append(new_div);
+                return;
             }
             //1st card clicked is not equal to 2nd card
         } else {
             //set flag variable to false which prevents additional clicks when card clicked is called
             canClick = false;
             setTimeout(function(){
-                //canClick = false;
                 $('.card').find('.notFlipped').show(10);
-                //$('.back').show(10);
-                canClick = true; // sets flag variable to true after 2000ms(2 seconds) so you can click the cards after the backs of the cards show
+                canClick = true; // sets flag variable to true AFTER 2000ms(2 seconds) so you can click the cards after the backs of the cards show
             }, 2000);
             first_card_clicked = null;
             second_card_clicked = null;
+            accuracy_percent(matches, attempts);
 
         } //end of else statement 1st card == 2nd card
 
-    } //end of else statement card=null
+        display_stats();
+    } //end of else statement 1st card=null
 
 } //end of function
+
+
+//create a function that calculates the accuracy which is % of matches/attempts
+function accuracy_percent(matches, attempts){
+    accuracy = (Math.floor(matches/attempts * 100)) + '%';
+}
+
+function display_stats(){
+    $('.games_played .value').text(games_played);
+    $('.attempts .value').text(attempts);
+    $('.accuracy .value').text(accuracy);
+}
+
+function reset_stats (){
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    display_stats();
+}
+
